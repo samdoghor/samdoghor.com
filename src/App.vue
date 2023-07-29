df2515d3d7e42ba015369d77a785d4d41edb1e28
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2023-07-29 10:55:41 +0100

    feat: setup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-
+    <div class="max-w-[100vw] max-h-screen mx-auto bg-gradient-to-r from-white via-zinc-200 to-white">
+            <router-link to="/"> <span> Home </span></router-link>
+            <a href="https://www.app.samdoghor.com" target="_blank"> <span> Portfolio </span></a>
+            <router-link to="/blog"> <span> Blog </span></router-link>
+            <router-link to="/courses"> <span> Courses </span></router-link>
+        <router-view/>
+    </div>
 </template>
